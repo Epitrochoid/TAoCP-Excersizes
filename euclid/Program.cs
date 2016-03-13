87b50f086b2ab6fd521b7de2c6d9aa74960e3d8e
Mabry Cervin <mpcervin@uncg.edu>
2016-03-13 17:02:22 -0400

    Added missing changes from previous commit

--- euclid/Program.cs
+++ euclid/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace euclid
-{
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
+namespace euclid {
+	
+	class EuclidAlgorithm {
+		public static int GreatestCommonDivisor(int m, int n) {
+			// Require that both m and n be positive
+			if (m < 0 || n < 0) {
+				throw new ArgumentException ();
+			}
+
+			int r = m % n; // E1 pg2
+
+			if (r == 0) {
+				return n; // E2
+			} else {
+				return GreatestCommonDivisor (n, r); // E3
+			}
+		}
+	}
+
+	class Euclid {
+		static void Main() {
+			int result = EuclidAlgorithm.GreatestCommonDivisor(35, 110);
+
+			Console.Write(result);
+
+			int fail = EuclidAlgorithm.GreatestCommonDivisor(-3, 22);
 		}
 	}
 }
